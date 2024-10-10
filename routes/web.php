7344f372eecf785c91a9d8421aa98f0061e6effc
Prath@7057 <iamprathpatil@gmail.com>
2024-10-10 23:35:33 +0530

    commite by prathamesh

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::resource('/',UserController::class);
 Route::resource('/user',UserController::class);
 Route::resource('/user/{user_id}',UserController::class);
